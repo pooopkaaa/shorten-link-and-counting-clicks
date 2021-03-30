68682e8bc22fbc2eb7303c5bbefda0a81f7f12de
pooopkaaa <60960322+pooopkaaa@users.noreply.github.com>
2021-03-30 21:43:43 +0300

    Add function to find bitlink

--- main.py
+++ main.py
@@ -5,15 +5,22 @@ import pprint
 env = Env()
 env.read_env()
 
-BITLY_ACCESS_TOKEN = env('BITLY_ACCESS_TOKEN')
-REAL_LINK = 'http://google.com'
 
-headers = {'Authorization': f'Bearer {BITLY_ACCESS_TOKEN}'}
-payload = {'long_url': f'{REAL_LINK}', }
-url = 'https://api-ssl.bitly.com/v4/bitlinks'
+def shorten_link(token, real_link):
+    headers = {'Authorization': f'Bearer {token}'}
+    payload = {'long_url': f'{real_link}', }
+    url = 'https://api-ssl.bitly.com/v4/bitlinks'
+    response = requests.post(url, headers=headers, json=payload)
+    response.raise_for_status()
+    bitlink = response.json()['link']
+    return bitlink
 
-response = requests.post(url, headers=headers, json=payload)
-response.raise_for_status()
 
-bitlink = response.json()['link']
-print(bitlink)
+def main():
+    token = env('BITLY_ACCESS_TOKEN')
+    real_link = 'http://google.com'
+    bitlink = shorten_link(token, real_link)
+    print(bitlink)
+
+if __name__ == '__main__':
+    main()
