8fd5388797814ac620bb7a41ab8894261a5875de
pooopkaaa <60960322+pooopkaaa@users.noreply.github.com>
2021-03-31 19:21:44 +0300

    Add function to count clicks

--- main.py
+++ main.py
@@ -12,20 +12,34 @@ def shorten_link(token, real_link):
     url = 'https://api-ssl.bitly.com/v4/bitlinks'
     response = requests.post(url, headers=headers, json=payload)
     response.raise_for_status()
-    pprint.pprint(response.json())
-    bitlink = response.json()['link']
+    bitlink = response.json()['id']
     return bitlink
+
+
+def count_cliks(token, bitlink):
+    headers = {'Authorization': f'Bearer {token}'}
+    url = f'https://api-ssl.bitly.com/v4/bitlinks/{bitlink}/clicks/summary'
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    amount_cliks = response.json()['total_clicks']
+    return amount_cliks
 
 
 def main():
     token = env('BITLY_ACCESS_TOKEN')
     real_link = input('Введите ссылку для сокращения: ')
-    
+
     try:
         bitlink = shorten_link(token, real_link)
         print(bitlink)
     except requests.exceptions.HTTPError as error:
         exit("Неверная ссылка. Ошибка: {0}".format(error))
+
+    try:
+        amount_cliks = count_cliks(token, bitlink)
+        print(amount_cliks)
+    except requests.exceptions.HTTPError as error:
+        exit("Неверный bitlink. Ошибка: {0}".format(error))
 
 if __name__ == '__main__':
     main()
